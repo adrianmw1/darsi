4ed0700ddd00c5c127496168881347c87e72a3c5
adrianmw1 <adrianmichaelwilliams@gmail.com>
2013-02-26 20:03:38 -0500

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Darsi::Application.config.secret_token = 'c7485107e178cd7b2cba95395da67a91dfaab8c20bb04a799fc9b760f83dcf13c4144af6f2598cc566fb5306b0fb81810b54872f7607e3c06db7d0b24146531d'
